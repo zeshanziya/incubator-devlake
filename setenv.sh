0536fed9e97da20b5ea10d864075cf82cf0577ab
zeshanziya <zeshan.ziya@axelerant.com>
2025-04-09 11:48:38 +0530

    add example plugin to export

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export CGO_LDFLAGS="-Wl,-rpath,/usr/local/lib"
-export DEVLAKE_PLUGINS=bamboo,bitbucket,circleci,customize,dora,gitextractor,github,github_graphql,gitlab,jenkins,jira,org,pagerduty,refdiff,slack,sonarqube,trello,webhook
-exec "$SHELL"
+export DEVLAKE_PLUGINS=bamboo,bitbucket,circleci,customize,dora,gitextractor,github,github_graphql,gitlab,jenkins,jira,org,pagerduty,refdiff,slack,sonarqube,trello,webhook,example
